e46942a2f7075281dd171c208bdf4a62bdee7a99
condor0010 <condor0010@laptop>
2022-11-17 11:07:05 -0500

    removed some clutter

--- trying_stuff/c.py
+++
@@ -1,9 +0,0 @@
-from pwn import *
-elf = ELF('../bins/bin-21')
-
-# see if usefull functions are on the symbol table
-has_gets = 'gets' in elf.symbols
-has_win  = 'win' in elf.symbols
-
-if has_gets and has_win:
-    print("yay")
