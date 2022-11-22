7dce8168027e048c83e794ddb3840598ba8ec79b
condor0010 <condor0010@laptop>
2022-11-22 12:30:22 -0500

    should work

--- main.py
+++ main.py
@@ -1,13 +1,40 @@
-#!/bin/python
-from pwn import *
-import angr, angrop, sys
-import argparse
+'''
+* Return-to-Win
+* Return-to-System
+Return-to-Execve
+Return-to-Syscall
+Return-to-Libc (OneGadget)
+ROP Write Primitive
+Format Strings Stack Leak
+Format Strings Libc Leak
+Format Strings Write Primitive
+Format Strings GOT Overwrite
+'''
+from auto_exploit_analyzer import *
 
-# Argparse will replace this
-binary = sys.argv[1]
+path = './bins/'
+for binary in os.listdir(path):
+    print(binary+":")
 
-# Functions for handling arguments go here
+    analyze(path + binary)
+    
+    print("    Has /bin/bash ", has_binsh())
+    print("    Has flag.txt  ", has_flagtxt())
+    print("    Has gets      ", has_gets())
+    print("    Has win       ", has_win())
+    print("    Has system    ", has_system())
+    print("    Has printf    ", has_printf())
+    print("    Has overflow  ", has_buffoverflow())
+    print("    Has rop       ", has_rop())
+    print("    Has syscall   ", has_syscall())
+    
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    # Add argument parsing
+'''
+    if has_win():
+        print("ret2win")
+    if has_rop():
+        print("rop is shoehorned in")
+    if has_binsh():
+        print("ret2system")
+'''  
+
