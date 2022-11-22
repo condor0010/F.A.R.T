76ffcbdf0dff0718361e5755b08056ac31ff0365
condor0010 <condor0010@laptop>
2022-11-22 16:08:08 -0500

    in a class now

--- main.py
+++ main.py
@@ -14,20 +14,19 @@ from auto_exploit_analyzer import *
 
 path = './bins/'
 for binary in os.listdir(path):
+    analyzer = analyze(path + binary)
     print(binary+":")
-
-    analyze(path + binary)
-    
-    print("    Has /bin/bash ", has_binsh())
-    print("    Has flag.txt  ", has_flagtxt())
-    print("    Has gets      ", has_gets())
-    print("    Has win       ", has_win())
-    print("    Has system    ", has_system())
-    print("    Has printf    ", has_printf())
-    print("    Has overflow  ", has_buffoverflow())
-    print("    Has rop       ", has_rop())
-    print("    Has syscall   ", has_syscall())
-    print("    Has format    ", has_format())
+    #print(analyzer.test())
+    print("    Has /bin/bash ", analyzer.has_binsh())
+    print("    Has flag.txt  ", analyzer.has_flagtxt())
+    print("    Has gets      ", analyzer.has_gets())
+    print("    Has win       ", analyzer.has_win())
+    print("    Has system    ", analyzer.has_system())
+    print("    Has printf    ", analyzer.has_printf())
+    print("    Has overflow  ", analyzer.has_buffoverflow())
+    print("    Has rop       ", analyzer.has_rop())
+    print("    Has syscall   ", analyzer.has_syscall())
+    print("    Has format    ", analyzer.has_format())
 
 '''
     if has_win():
