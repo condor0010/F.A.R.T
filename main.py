a947e3758cab0464c11f8a7aab3d75ca8383e5c1
condor0010 <condor0010@laptop>
2022-11-28 22:37:00 -0500

    its less funcinal now

--- main.py
+++ main.py
@@ -11,32 +11,25 @@ Format Strings Write Primitive
 Format Strings GOT Overwrite
 '''
 import os
+from pwn import *
 from auto_exploit_analyzer import analyze
 
 path = './bins/'
 for binary in os.listdir(path):
     analyzer = analyze(path + binary)
     print(binary+":")
-    #print(analyzer.test())
     print("    Has /bin/bash ", analyzer.has_binsh())
     print("    Has flag.txt  ", analyzer.has_flagtxt())
     print("    Has gets      ", analyzer.has_gets())
     print("    Has win       ", analyzer.has_win())
     print("    Has system    ", analyzer.has_system())
     print("    Has printf    ", analyzer.has_printf())
-    print("    Has overflow  ", analyzer.has_buffoverflow())
+    #print("    Has overflow  ", analyzer.has_buffoverflow())
     print("    Has rop       ", analyzer.has_rop())
     print("    Has syscall   ", analyzer.has_syscall())
     print("    Has format    ", analyzer.has_format())
     print("    Has execve    ", analyzer.has_execve())
-
-
-'''
-    if has_win():
-        print("ret2win")
-    if has_rop():
-        print("rop is shoehorned in")
-    if has_binsh():
-        print("ret2system")
-'''  
+    print("        /bin/sh   ", analyzer.get_binsh())
+    print("        flag.txt  ", analyzer.get_flagtxt())
+    print("        win       ", analyzer.get_win())
 
