1fac69ce9291082a2aed151eb8841efb37edff4d
Nyt3_jmp <jconnolly80911@gmail.com>
2022-12-03 15:25:37 -0500

    First push for main

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
-#!/bin/python
+#!/bin/pythoni
 from pwn import *
-import angr, angrop, sys
-import argparse
+import sys
+from fart import *
 
-# Argparse will replace this
-binary = sys.argv[1]
-
-# Functions for handling arguments go here
+def check_vuln_type(binary):
+    
+    if binary.has_binsh():
+        print("Has binsh")
+    if binary.has_flagtxt():
+        print("Has flag.txt")
+    if binary.has_catflagtxt():
+        print("Has cat flag")
+    if binary.has_gets():
+        print("Has gets")
+    if binary.has_win():
+        print("Has win")
+    if binary.has_system():
+        print("Has system")
+    if binary.has_printf():
+        print("Has printf")
+    if binary.has_syscall():
+        print("Has syscall")
+    if binary.has_format():
+        print("Has format")
+    if binary.has_execve():
+        print("Has execve")
+    if binary.has_rop():
+        print("Has ROP")
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    # Add argument parsing
+    binary = args.BIN
+
+    analyze = Analyze(binary)
+    check_vuln_type(analyze)
+
